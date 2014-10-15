bf133d989e3861e3bc275bb46c41523a98df2778
Charles Holbrow <CharlesHolbrow@gmail.com>
2014-10-15 12:09:46 -0400

    Lock volume to 0.7

--- express/lucernePlayer.js
+++ express/lucernePlayer.js
@@ -11,6 +11,7 @@ SoundPlayer.prototype = {
 play: function(name, stereo){
   var number = stereo ? 0 : Math.floor(Math.random() * 9); // 0 to 8, inclusive
   var volume = Math.random() * 0.5 + 0.5;
+  volume = 0.7; // hack
   this.client.send('/' + name, volume, number, number + 1);
 }
 
