e1941e048e189191156a63af334bf480265a4794
Charles Holbrow <CharlesHolbrow@gmail.com>
2014-10-16 17:01:54 -0400

    clean some redundencies in webapp

--- express/webapp.js
+++ express/webapp.js
@@ -1,23 +1,19 @@
-var bodyParser = require('body-parser');
+// npm modules
 var express = require('express');
+var bodyParser = require('body-parser');
 
+// local modules
 var SoundPlayer = require('./lucernePlayer.js');
 var player = new SoundPlayer();
 
 var app = express();
-app.use(bodyParser());
-app.use('/static', express.static(__dirname + '/static'));
-app.use('/img', express.static(__dirname + '/static/img'));
-app.use('/js', express.static(__dirname + '/static/js'));
+app.use(express.static(__dirname + '/static'));
 app.use('/json', express.static(__dirname + '/../'));
+app.use(bodyParser());
 
 // hack
 var osc = require('node-osc');
 client = new osc.Client('127.0.0.1', 3334);
-
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/static/index.html');
-});
 
 app.post('/off', function(req, res){
   client.send('/off');
